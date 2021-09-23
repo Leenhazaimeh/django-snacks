d6f60b078c88a1b21a172840933c546126fa598a
Leen Hazaimeh <leenhazaimeh94@gmail.coom>
2021-09-23 11:03:00 +0300

    fix the home and about pages urls and adde the missing test

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,21 +1,32 @@
 from django.test import SimpleTestCase
 from django.urls import reverse
 
-# Create your tests here.
-class thingsTests(SimpleTestCase):
-    def test_home_page_status_code(self):
-        url = reverse('home')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
 
-    def test_about_page_status_code(self):
-        url = reverse('about')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
 
     
-    def test_home_page_templete(self):
-        url = reverse('home')
-        response = self.client.get(url)
-        self.assertTemplateUsed(response, 'home.html')
-        self.assertTemplateUsed(response, 'base.html')
+def test_home_page_templete(self):
+            url = reverse('home')
+            response = self.client.get(url)
+            self.assertTemplateUsed(response, 'home.html')
+            self.assertTemplateUsed(response, 'base.html')
+
+def test_home_status(self):
+            url = reverse('home')
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+def test_about_status(self):
+            url = reverse('about')
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+def test_base_template(self):
+            url = reverse('home')
+            response = self.client.get(url)
+            self.assertTemplateUsed(response, 'base.html')
+def test_home_template(self):
+            url = reverse('home')
+            response = self.client.get(url)
+            self.assertTemplateUsed(response, 'home.html')
+def test_about_template(self):
+            url = reverse('about')
+            response = self.client.get(url)
+            self.assertTemplateUsed(response, 'about.html')
